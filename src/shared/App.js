3d3b1177d64f8278439e5d3211b938d3f53b97bb
jjubbu <hogugugut4102.off@gmail.com>
2021-10-12 19:21:09 +0900

    edit:안쓰는 코드제거

--- src/shared/App.js
+++ src/shared/App.js
@@ -1,16 +1,13 @@
 import React from "react";
-import { useSelector , useDispatch} from "react-redux";
+
 import User from "../pages/User";
 import Main from "../pages/Main";
 import { ConnectedRouter } from "connected-react-router";
 import { history } from "../redux/configureStore";
 import { Route } from "react-router";
 import {apis} from "../lib/axios"
-import {actionCreators as userAction} from "../redux/modules/user"
 
 function App() {
-  // const dispatch = useDispatch();
-  // const is_login = useSelector(state => state.user.is_login)
 
   React.useEffect(()=>{
     apis.logInAX().then((res)=>{
@@ -19,7 +16,6 @@ function App() {
       }else{
         history.push('/login');
       }
-      console.log(res.data[0].msg);
   })
   },[])
 
