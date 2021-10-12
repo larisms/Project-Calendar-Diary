3c3d63102b21d4d6d3b0b17eef7fabe99daeb397
jjubbu <hogugugut4102.off@gmail.com>
2021-10-12 19:36:59 +0900

    edit: 로그인 깜빡이지 않게

--- src/shared/App.js
+++ src/shared/App.js
@@ -8,20 +8,23 @@ import { Route } from "react-router";
 import {apis} from "../lib/axios"
 
 function App() {
-
+  const [is_login, setstate] = React.useState('false');
+  
   React.useEffect(()=>{
     apis.logInAX().then((res)=>{
       if(res.data.msg === "success"){
+          setstate(true);
           history.push('/');
       }else{
-        history.push('/login');
+          history.push('/login');
+          setstate(false);
       }
   })
   },[])
 
   return (
     <ConnectedRouter history={history}>
-      <Route path="/login" exact component={User}/>
+      {!is_login? <Route path="/login" exact component={User}/> : null}
       <Route path="/" exact component={Main}/>
     </ConnectedRouter>
   );
