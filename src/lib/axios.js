93c02c36d0c5549dd71a2c1c3cc51a622b9a3a86
jjubbu <hogugugut4102.off@gmail.com>
2021-10-14 13:47:09 +0900

    edit:쿠키 보내는 코드 추가

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -6,7 +6,7 @@ const cookies = new Cookies();
 
 const instance = axios.create({
   // 기본적으로 우리가 바라볼 서버의 주소
-  baseURL: "http://localhost:4000/",
+  baseURL: "http://3.36.99.138:4000/",
   headers: {
     "content-type": "application/json;charset=UTF-8",
     accept: "application/json",
